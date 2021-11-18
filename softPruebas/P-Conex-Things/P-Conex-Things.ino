59f30a5f7360f5ecdfe39b6edbc78ba0f0f9dcc6
Juan Arquero <j.arquerog@gmail.com>
2021-11-18 18:55:55 +0100

    Falta revisar funcion de transformacion de voltajea turbidez

--- softPruebas/P-Conex-Things/P-Conex-Things.ino
+++ softPruebas/P-Conex-Things/P-Conex-Things.ino
@@ -3,20 +3,28 @@
 #include <DallasTemperature.h>
 #include <ThingsBoard.h>
 #include <WiFi.h>
+#include "esp_adc_cal.h"
 
-#define WIFI_AP_NAME        "RTF"
-#define WIFI_PASSWORD       "desconectado"
+#define WIFI_AP_NAME        "LaZona"
+#define WIFI_PASSWORD       "12345678"
 #define TOKEN               "ESP32SBC"
 #define THINGSBOARD_SERVER  "demo.thingsboard.io"
 #define SERIAL_DEBUG_BAUD 115200
-#define PIN_TURBIDEZ 25
+#define PIN_TURBIDEZ 32
+#define FILTER_LEN  15
+ 
+uint32_t AN_Pot1_Buffer[FILTER_LEN] = {0};
+int AN_Pot1_i = 0;
+int AN_Pot1_Filtered = 0;
 
 const int pinDatosDQ= 33;
 int status = WL_IDLE_STATUS;
 float TempI=0.0;
 float TempE=0.0;
 float difTemp=0.0;
+float calTurb=0.0;
 float turbidez=0.0;
+
 
 OneWire oneWireObjeto(pinDatosDQ);
 DallasTemperature sensorDS18B20(&oneWireObjeto);
@@ -69,8 +77,21 @@ void getTurbidez(){
     //analogSetSamples(8);
     analogReadResolution(12);
     int rawTurb = analogRead(PIN_TURBIDEZ);
-    turbidez=(-1120.4*(float)rawTurb*(float)rawTurb + 5742.3*(float)rawTurb-4352.9 );
+    
+    //Serial.println(turbidez);
+    calTurb = getCalibrated(rawTurb)/1000.0;
+    AN_Pot1_Filtered=readADC_Avg(calTurb);
+    //float vol=(calTurb*3.3)/4095;
+    turbidez= -2572.2*calTurb*calTurb + 8700.5*calTurb - 4352.9 ;
+    Serial.println("Valor sin calibrar:");
+    Serial.println(rawTurb);
+    Serial.println("Valor Calibrado");
+    Serial.println(calTurb);
+    Serial.println("Valor Calibrado Multi");
+    Serial.println(AN_Pot1_Filtered);
+    Serial.println("Valor Sensor");
     Serial.println(turbidez);
+    
   }
 
 void getTemperature(){
@@ -109,7 +130,31 @@ void sendData(){
   tb.sendTelemetryFloat("Temperatura Exterior", TempE);
   tb.sendTelemetryFloat("Temperatura Interior", TempI);
   tb.sendTelemetryFloat("Diferencia Temp", difTemp);
-  tb.sendTelemetryFloat("Turbidez", turbidez);
+  tb.sendTelemetryFloat("Turbidez", calTurb);
   
   tb.loop(); //Esta instrucción ha de ser la última del método
   }
+
+uint32_t getCalibrated(int ADC_Raw){
+  esp_adc_cal_characteristics_t adc_chars;
+  
+  esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12, 1000, &adc_chars);
+  return(esp_adc_cal_raw_to_voltage(ADC_Raw, &adc_chars));
+  }
+
+  uint32_t readADC_Avg(int ADC_Raw)
+{
+  int i = 0;
+  uint32_t Sum = 0;
+  
+  AN_Pot1_Buffer[AN_Pot1_i++] = ADC_Raw;
+  if(AN_Pot1_i == FILTER_LEN)
+  {
+    AN_Pot1_i = 0;
+  }
+  for(i=0; i<FILTER_LEN; i++)
+  {
+    Sum += AN_Pot1_Buffer[i];
+  }
+  return (Sum/FILTER_LEN);
+}
